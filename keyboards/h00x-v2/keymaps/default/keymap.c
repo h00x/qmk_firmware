d798d4306f4ef49ab8ff4e0faf2b6f49af777d7c
Dave Hoeks <dave.hoeks@gmail.com>
2019-10-27 12:51:19 +0100

    adds keyboards

---
+++ keyboards/h00x-v2/keymaps/default/keymap.c
@@ -0,0 +1,12 @@
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+	LAYOUT(
+			KC_0, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_1, KC_2, KC_3,
+			KC_0, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_1, KC_2, KC_3,
+			KC_0, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_1, KC_2,
+			KC_0, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_1,
+			KC_0, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7
+		)
+};
