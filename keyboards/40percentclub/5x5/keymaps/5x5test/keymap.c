594f99bd2374ac76d9e79d766cc365af54a4c657
Dave Hoeks <dave.hoeks@gmail.com>
2019-10-27 12:51:19 +0100

    adds keyboards

--- keyboards/40percentclub/5x5/keymaps/5x5test/keymap.c
+++ keyboards/40percentclub/5x5/keymaps/5x5test/keymap.c
@@ -2,26 +2,12 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-/* Single 5x5 board only
- * .--------------------------------------------.
- * | QWERTY |   /    |   *    |   -    |        |
- * |--------+--------+--------+--------+--------|
- * |   7    |   8    |   9    |   +    |        |
- * |--------+--------+--------+--------+--------|
- * |   4    |   5    |   6    |   +    |        |
- * |--------+--------+--------+--------+--------|
- * |   1    |   2    |   3    | ENTER  |        |
- * |--------+--------+--------+--------+--------|
- * |   0    |   0    |   .    | ENTER  |        |
- * '--------------------------------------------'
- */
-
-  LAYOUT_ortho_5x5(
-  KC_KP_1, KC_KP_2, KC_KP_3,   KC_KP_4, KC_KP_5,
-  KC_KP_1, KC_KP_2, KC_KP_3,   KC_KP_4, KC_KP_5,
-  KC_KP_1, KC_KP_2, KC_KP_3,   KC_KP_4, KC_KP_5,
-  KC_KP_1, KC_KP_2, KC_KP_3,   KC_KP_4, KC_KP_5,
-  KC_KP_1, KC_KP_2, KC_KP_3,   KC_KP_4, KC_KP_5
+  LAYOUT_ortho_5x15(
+  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,
+  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,
+  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,
+  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,
+  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5
   ),
 
 };
